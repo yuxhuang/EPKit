fc2e1c9c39dcb77fa3992d68f4455102de33546b
Felix Huang <gzfelix@gmail.com>
2014-03-12 21:47:03 -0400

    initial commit

---
+++ EPKit.ObjectGraph/EPKit.ObjectGraph.Example/Infrastructure/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Owin;
+using System.Web.Http;
+
+namespace EPKit.ObjectGraph.Example.Infrastructure
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder appBuilder)
+		{
+			// Configure Web API for self-host. 
+			HttpConfiguration config = new HttpConfiguration(); 
+			config.Routes.MapHttpRoute( 
+				name: "DefaultApi", 
+				routeTemplate: "api/{controller}/{id}", 
+				defaults: new { id = RouteParameter.Optional } 
+			); 
+
+			appBuilder.UseWebApi(config); 
+		}
+	}
+}
+
